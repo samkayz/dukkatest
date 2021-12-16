2716770de910a30a6b24a61b8ddd5e59446ec1bd
finedevsam <sam.ilemobayo@gmail.com>
2021-12-16 14:54:03 +0100

    Static

--- dukka/settings.py
+++ dukka/settings.py
@@ -155,10 +155,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+
 
 #Media Directory
 MEDIA_URL = '/media/'
